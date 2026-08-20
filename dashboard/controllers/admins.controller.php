846d69dbdb609cccfb284a71d9087fec8925812d
puri calvo <puri_adrian@hotmail.com>
2026-08-20 09:15:27 +0200

    Cambio en AdminController securityCode

--- dashboard/controllers/admins.controller.php
+++ dashboard/controllers/admins.controller.php
@@ -164,7 +164,7 @@ class AdminsController{
 					localStorage.setItem("tokenAdmin","'.$admin->results[0]->token_admin.'");
 					fncMatPreloader("off");
 					fncFormatInputs();
-					location.reload();
+					window.location.href = "/";
 
 				</script>';
 
